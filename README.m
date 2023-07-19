04ee1dcb34b21a2dab77505278e1b27ff7eadc4b
ramegnam <140001918+ramegnam@users.noreply.github.com>
2023-07-19 19:52:47 +0300

    Create README.m
    ~# apt install wifiphisher
    
    Reading package lists... Done
    Building dependency tree
    Reading state information... Done
    wifiphisher is already the newest version (1.4+git20191215-0kali1).
    The following packages were automatically installed and are no longer required:
      dh-python libdouble-conversion1 liblinear3
    Use 'apt autoremove' to remove them.
    0 upgraded, 0 newly installed, 0 to remove and 1

---
+++ README.m
@@ -0,0 +1,20 @@
+- I’m @ramegnam
+- 👀 I’m interested in ...
+- 🌱 I’m currently learning ...
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ...
+
+<!---
+ramegnam/ramegnam is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
+~# apt install wifiphisher
+
+Reading package lists... Done
+Building dependency tree
+Reading state information... Done
+wifiphisher is already the newest version (1.4+git20191215-0kali1).
+The following packages were automatically installed and are no longer required:
+  dh-python libdouble-conversion1 liblinear3
+Use 'apt autoremove' to remove them.
+0 upgraded, 0 newly installed, 0 to remove and 1
